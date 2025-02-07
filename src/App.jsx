6a40ad9e4c6aba49c7d263ed0f8130ebb99b97fb
Imantha7 <imanthapiyumal7@gmail.com>
2025-02-07 11:04:24 +0530

    init 🎈

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-red-700">
+      <h1 className="text-white text-3xl font-bold">
+        kv-audio-frontend
+      </h1>
+    </div>
+  )
+}
+
+export default App
