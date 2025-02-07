216188088ff405c5970fd9985560897598b71ede
Imantha7 <imanthapiyumal7@gmail.com>
2025-02-07 11:05:43 +0530

    added tailwind css 🔥

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex items-center justify-center w-full h-screen bg-red-700">
       <h1 className="text-white text-3xl font-bold">
-        kv-audio-frontend
+            kv-audio-frontend
       </h1>
     </div>
   )
